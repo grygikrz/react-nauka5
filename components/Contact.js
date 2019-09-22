8f5f7449a96e14c65d5d35365c2250db07e9edd3
Krzysiek <kgrygiel@gmail.com>
2019-09-22 15:58:40 +0200

    test jsx script2

--- components/Contact.js
+++ components/Contact.js
@@ -1,21 +1,9 @@
-var Contact = React.createClass({
-  propTypes: {
-    item: React.PropTypes.object.isRequired,
-  },
-
-  render: function() {
-    return (
-      React.createElement('div', {className: 'contactItem'},
-        React.createElement('img', {
-          className: 'contactImage',
-          src: 'http://files.softicons.com/download/android-icons/flat-icons-by-martz90/png/64x64/contacts.png'
-        }),
-        React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-        React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-        React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
-          this.props.item.email
-        )
-      )
-    )
-  },
-});
+<div className={'contactItem'}>
+    <img className={'contactImage'} src={'link-do-obrazka.png'}/>
+    <p className={'contactLabel'}>
+        Imię: {this.props.contact.firstName}
+    </p>
+    <a href={'mailto:' + this.props.item.email}>
+        {this.props.item.email}
+    </a>
+</div>
